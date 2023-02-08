80a5c5d7bdee53a0d0be0d90788488bdc6cbedd2
Biplob <biplob.kazsoft@gmail.com>
2023-02-08 15:56:50 +0600

    update server starting console

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(5000, () => {
+    console.log('App running on port 5000');
+  });
 }
 bootstrap();
